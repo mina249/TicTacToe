1d3f5dee8a3aa248d1c923514c673aa1b099a06b
AHMHAM <ahmedhambouta@gmail.com>
2022-12-30 20:18:59 +0200

    UI Edited OnlinePlayerList

--- ClientTicTacToe/src/clienttictactoe/OnlinePlayerListBase.java
+++ ClientTicTacToe/src/clienttictactoe/OnlinePlayerListBase.java
@@ -29,8 +29,22 @@ public abstract class OnlinePlayerListBase extends GridPane {
     protected final RowConstraints rowConstraints2;
     protected final FlowPane flowPane;
     protected final Label labelLocalPlayerName;
+    protected final Label labelLocalPlayerName1;
+    protected final Label labelLocalPlayerName2;
+    protected final Label labelLocalPlayerName3;
+    protected final Label labelLocalPlayerName4;
+    protected final Label labelLocalPlayerName5;
+    protected final Label labelLocalPlayerName6;
+    protected final Label labelLocalPlayerName7;
     protected final FlowPane flowPane0;
     protected final Button buttonChallenge;
+    protected final Button buttonChallenge1;
+    protected final Button buttonChallenge2;
+    protected final Button buttonChallenge3;
+    protected final Button buttonChallenge4;
+    protected final Button buttonChallenge5;
+    protected final Button buttonChallenge6;
+    protected final Button buttonChallenge7;
 
     public OnlinePlayerListBase() {
 
@@ -52,8 +66,22 @@ public abstract class OnlinePlayerListBase extends GridPane {
         rowConstraints2 = new RowConstraints();
         flowPane = new FlowPane();
         labelLocalPlayerName = new Label();
+        labelLocalPlayerName1 = new Label();
+        labelLocalPlayerName2 = new Label();
+        labelLocalPlayerName3 = new Label();
+        labelLocalPlayerName4 = new Label();
+        labelLocalPlayerName5 = new Label();
+        labelLocalPlayerName6 = new Label();
+        labelLocalPlayerName7 = new Label();
         flowPane0 = new FlowPane();
         buttonChallenge = new Button();
+        buttonChallenge1 = new Button();
+        buttonChallenge2 = new Button();
+        buttonChallenge3 = new Button();
+        buttonChallenge4 = new Button();
+        buttonChallenge5 = new Button();
+        buttonChallenge6 = new Button();
+        buttonChallenge7 = new Button();
 
         setMaxHeight(USE_PREF_SIZE);
         setMaxWidth(USE_PREF_SIZE);
@@ -61,7 +89,7 @@ public abstract class OnlinePlayerListBase extends GridPane {
         setMinWidth(USE_PREF_SIZE);
         setPrefHeight(600.0);
         setPrefWidth(1200.0);
-        setStyle("-fx-background-color: RGB(31,49,115);");
+        setStyle("-fx-background-color: #1F3274;");
 
         columnConstraints.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
         columnConstraints.setMinWidth(10.0);
@@ -77,16 +105,16 @@ public abstract class OnlinePlayerListBase extends GridPane {
         rowConstraints0.setVgrow(javafx.scene.layout.Priority.SOMETIMES);
 
         columnConstraints0.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
-        columnConstraints0.setMinWidth(5.0);
+        columnConstraints0.setMinWidth(15.0);
 
         columnConstraints1.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
         columnConstraints1.setMinWidth(0.0);
-        columnConstraints1.setPercentWidth(55.0);
+        columnConstraints1.setPercentWidth(70.0);
         columnConstraints1.setPrefWidth(100.0);
 
         columnConstraints2.setHgrow(javafx.scene.layout.Priority.SOMETIMES);
-        columnConstraints2.setMinWidth(15.0);
-        columnConstraints2.setPercentWidth(20.0);
+        columnConstraints2.setMinWidth(0.0);
+        columnConstraints2.setPercentWidth(10.0);
 
         rowConstraints1.setMinHeight(10.0);
         rowConstraints1.setPrefHeight(30.0);
@@ -94,26 +122,27 @@ public abstract class OnlinePlayerListBase extends GridPane {
 
         buttonBack.setMnemonicParsing(false);
         buttonBack.setPrefHeight(40.0);
-        buttonBack.setStyle("-fx-background-color: RGB(31,49,115);");
+        buttonBack.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 30;");
         buttonBack.setText("<");
-        buttonBack.setTextFill(javafx.scene.paint.Color.valueOf("#5a5a5a"));
+        buttonBack.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
         buttonBack.setFont(new Font("Arial Bold", 36.0));
         GridPane.setMargin(buttonBack, new Insets(0.0, 0.0, 0.0, 10.0));
 
         GridPane.setColumnIndex(buttonRecord, 2);
         buttonRecord.setMnemonicParsing(false);
         buttonRecord.setPrefHeight(40.0);
-        buttonRecord.setStyle("-fx-background-color: RGB(255,73,72);");
+        buttonRecord.setStyle("-fx-background-color: #FF4948;");
         buttonRecord.setText("Record");
         buttonRecord.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
         buttonRecord.setFont(new Font("Showcard Gothic", 12.0));
+        buttonRecord.setPadding(new Insets(0.0, 20.0, 0.0, 20.0));
 
         GridPane.setColumnIndex(label, 1);
         label.setMinHeight(0.0);
         label.setMinWidth(0.0);
         label.setText("Your Online Friends");
-        label.setTextFill(javafx.scene.paint.Color.valueOf("#ff4a4a"));
-        label.setFont(new Font("Showcard Gothic", 24.0));
+        label.setTextFill(javafx.scene.paint.Color.valueOf("#ff4948"));
+        label.setFont(new Font("Showcard Gothic", 40.0));
 
         GridPane.setRowIndex(gridPane0, 1);
 
@@ -140,11 +169,84 @@ public abstract class OnlinePlayerListBase extends GridPane {
         flowPane.setPrefWidth(200.0);
 
         labelLocalPlayerName.setPrefHeight(40.0);
-        labelLocalPlayerName.setStyle("-fx-background-color: RGB(90,90,90);");
+        labelLocalPlayerName.setPrefWidth(500.0);
+        labelLocalPlayerName.setStyle("-fx-background-color: #5A5A5A;");
         labelLocalPlayerName.setText("Player Name");
-        labelLocalPlayerName.setTextFill(javafx.scene.paint.Color.valueOf("#fd9727"));
+        labelLocalPlayerName.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
         FlowPane.setMargin(labelLocalPlayerName, new Insets(0.0, 0.0, 15.0, 0.0));
         labelLocalPlayerName.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName.setOpaqueInsets(new Insets(0.0));
+
+        labelLocalPlayerName1.setPrefHeight(40.0);
+        labelLocalPlayerName1.setPrefWidth(500.0);
+        labelLocalPlayerName1.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName1.setText("Player Name");
+        labelLocalPlayerName1.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName1.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName1.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName1.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName1, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName2.setPrefHeight(40.0);
+        labelLocalPlayerName2.setPrefWidth(500.0);
+        labelLocalPlayerName2.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName2.setText("Player Name");
+        labelLocalPlayerName2.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName2.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName2.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName2.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName2, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName3.setPrefHeight(40.0);
+        labelLocalPlayerName3.setPrefWidth(500.0);
+        labelLocalPlayerName3.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName3.setText("Player Name");
+        labelLocalPlayerName3.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName3.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName3.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName3.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName3, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName4.setPrefHeight(40.0);
+        labelLocalPlayerName4.setPrefWidth(500.0);
+        labelLocalPlayerName4.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName4.setText("Player Name");
+        labelLocalPlayerName4.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName4.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName4.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName4.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName4, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName5.setPrefHeight(40.0);
+        labelLocalPlayerName5.setPrefWidth(500.0);
+        labelLocalPlayerName5.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName5.setText("Player Name");
+        labelLocalPlayerName5.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName5.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName5.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName5.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName5, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName6.setPrefHeight(40.0);
+        labelLocalPlayerName6.setPrefWidth(500.0);
+        labelLocalPlayerName6.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName6.setText("Player Name");
+        labelLocalPlayerName6.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName6.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName6.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName6.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName6, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        labelLocalPlayerName7.setPrefHeight(40.0);
+        labelLocalPlayerName7.setPrefWidth(500.0);
+        labelLocalPlayerName7.setStyle("-fx-background-color: #5A5A5A;");
+        labelLocalPlayerName7.setText("Player Name");
+        labelLocalPlayerName7.setTextFill(javafx.scene.paint.Color.valueOf("#febf00"));
+        labelLocalPlayerName7.setFont(new Font("System Bold", 14.0));
+        labelLocalPlayerName7.setPadding(new Insets(0.0, 200.0, 0.0, 200.0));
+        labelLocalPlayerName7.setOpaqueInsets(new Insets(0.0));
+        FlowPane.setMargin(labelLocalPlayerName7, new Insets(0.0, 0.0, 15.0, 0.0));
 
         GridPane.setColumnIndex(flowPane0, 2);
         flowPane0.setOrientation(javafx.geometry.Orientation.VERTICAL);
@@ -153,11 +255,67 @@ public abstract class OnlinePlayerListBase extends GridPane {
 
         buttonChallenge.setMnemonicParsing(false);
         buttonChallenge.setPrefHeight(40.0);
-        buttonChallenge.setStyle("-fx-background-color: RGB(255,73,72);");
+        buttonChallenge.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
         buttonChallenge.setText("Challenge");
         buttonChallenge.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
         FlowPane.setMargin(buttonChallenge, new Insets(0.0, 0.0, 15.0, 0.0));
         buttonChallenge.setFont(new Font("System Bold", 14.0));
+
+        buttonChallenge1.setMnemonicParsing(false);
+        buttonChallenge1.setPrefHeight(40.0);
+        buttonChallenge1.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge1.setText("Challenge");
+        buttonChallenge1.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge1.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge1, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge2.setMnemonicParsing(false);
+        buttonChallenge2.setPrefHeight(40.0);
+        buttonChallenge2.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge2.setText("Challenge");
+        buttonChallenge2.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge2.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge2, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge3.setMnemonicParsing(false);
+        buttonChallenge3.setPrefHeight(40.0);
+        buttonChallenge3.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge3.setText("Challenge");
+        buttonChallenge3.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge3.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge3, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge4.setMnemonicParsing(false);
+        buttonChallenge4.setPrefHeight(40.0);
+        buttonChallenge4.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge4.setText("Challenge");
+        buttonChallenge4.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge4.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge4, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge5.setMnemonicParsing(false);
+        buttonChallenge5.setPrefHeight(40.0);
+        buttonChallenge5.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge5.setText("Challenge");
+        buttonChallenge5.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge5.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge5, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge6.setMnemonicParsing(false);
+        buttonChallenge6.setPrefHeight(40.0);
+        buttonChallenge6.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge6.setText("Challenge");
+        buttonChallenge6.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge6.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge6, new Insets(0.0, 0.0, 15.0, 0.0));
+
+        buttonChallenge7.setMnemonicParsing(false);
+        buttonChallenge7.setPrefHeight(40.0);
+        buttonChallenge7.setStyle("-fx-background-color: #FF4948; -fx-background-radius: 0;");
+        buttonChallenge7.setText("Challenge");
+        buttonChallenge7.setTextFill(javafx.scene.paint.Color.valueOf("#d9d9d9"));
+        buttonChallenge7.setFont(new Font("System Bold", 14.0));
+        FlowPane.setMargin(buttonChallenge7, new Insets(0.0, 0.0, 15.0, 0.0));
 
         getColumnConstraints().add(columnConstraints);
         getRowConstraints().add(rowConstraints);
@@ -175,8 +333,22 @@ public abstract class OnlinePlayerListBase extends GridPane {
         gridPane0.getColumnConstraints().add(columnConstraints5);
         gridPane0.getRowConstraints().add(rowConstraints2);
         flowPane.getChildren().add(labelLocalPlayerName);
+        flowPane.getChildren().add(labelLocalPlayerName1);
+        flowPane.getChildren().add(labelLocalPlayerName2);
+        flowPane.getChildren().add(labelLocalPlayerName3);
+        flowPane.getChildren().add(labelLocalPlayerName4);
+        flowPane.getChildren().add(labelLocalPlayerName5);
+        flowPane.getChildren().add(labelLocalPlayerName6);
+        flowPane.getChildren().add(labelLocalPlayerName7);
         gridPane0.getChildren().add(flowPane);
         flowPane0.getChildren().add(buttonChallenge);
+        flowPane0.getChildren().add(buttonChallenge1);
+        flowPane0.getChildren().add(buttonChallenge2);
+        flowPane0.getChildren().add(buttonChallenge3);
+        flowPane0.getChildren().add(buttonChallenge4);
+        flowPane0.getChildren().add(buttonChallenge5);
+        flowPane0.getChildren().add(buttonChallenge6);
+        flowPane0.getChildren().add(buttonChallenge7);
         gridPane0.getChildren().add(flowPane0);
         getChildren().add(gridPane0);
 
