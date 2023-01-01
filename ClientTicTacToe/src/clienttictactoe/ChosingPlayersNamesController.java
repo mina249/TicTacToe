637abee9c9f11514fd730dd85418cce5bd17469b
asmaa727 <engineerasmaa2019@gmail.com>
2023-01-02 01:14:23 +0200

    creating choose players names and game records gui

---
+++ ClientTicTacToe/src/clienttictactoe/ChosingPlayersNamesController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienttictactoe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nada
+ */
+public class ChosingPlayersNamesController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
