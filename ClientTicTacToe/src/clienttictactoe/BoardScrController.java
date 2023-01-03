5055bc7f85661282631efbb1608605ca1db0d88d
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2023-01-03 07:20:08 -0800

    in BoardScrController add setStandrdStart,highlightTheLabel,handleNoOneWin&handleWinningSomeOne modify handleWinningSomeOne,highlightThusBtns,revieweTheBoaed(that was named checkStatus), isAnyDiagonalNotifyWinningSomeone,isAnyColumnNotifyWinningSomeone,isAnyRowNotifyWinningSomeone handleThePressedBtnTwoPlayersMood,handlePlayAgainBtn,notifyPressing,handleBackButtonAction&initialize in addition of modify the font of the board's btn and re-organize the board's btn and dimentions

--- ClientTicTacToe/src/clienttictactoe/BoardScrController.java
+++ ClientTicTacToe/src/clienttictactoe/BoardScrController.java
@@ -17,7 +17,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -62,17 +65,20 @@ public class BoardScrController implements Initializable {
     private int firstPlayerScore = 0;
     private int secondPlayerScore = 0;
     private String winnerName;
+    private String gameStatus;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        highlightTheLabel('o');
     }    
     
     @FXML
     private void handleBackButtonAction(ActionEvent event) {
-          try {
+        gameStatus = "terminated";
+        try {
             Parent root = FXMLLoader.load(getClass().getResource("HomePageScreen.fxml"));
             Scene scene = new Scene(root);
             Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
@@ -84,34 +90,28 @@ public class BoardScrController implements Initializable {
     }
     @FXML
     private void notifyPressing(ActionEvent event) {
-        handleThePressedBtn(event);
+        handleThePressedBtnTwoPlayersMood(event);
     }
     @FXML
     private void handlePlayAgainBtn(ActionEvent event) {
-        box1.setText("");box1.setStyle("-fx-background-color: #1F3274; ");
-        box2.setText("");box2.setStyle("-fx-background-color: #1F3274; ");
-        box3.setText("");box3.setStyle("-fx-background-color: #1F3274; ");
-        box4.setText("");box4.setStyle("-fx-background-color: #1F3274; ");
-        box5.setText("");box5.setStyle("-fx-background-color: #1F3274; ");
-        box6.setText("");box6.setStyle("-fx-background-color: #1F3274; ");
-        box7.setText("");box7.setStyle("-fx-background-color: #1F3274; ");
-        box8.setText("");box8.setStyle("-fx-background-color: #1F3274; ");
-        box9.setText("");box9.setStyle("-fx-background-color: #1F3274; ");
-        someoneIsWin = false;
+        setStandrdStart();
     }
-    
-    public void handleThePressedBtn(ActionEvent e){
+    public void handleThePressedBtnTwoPlayersMood(ActionEvent e){
         if(!someoneIsWin){
             targetedBtn = (Button) e.getSource();
-            if(targetedBtn.getText().equals("")){
-                targetedBtn.setText(textOfBtn);                
+            if(targetedBtn.getText().equals("")){               
                 if(textOfBtn=="X"){
+                    targetedBtn.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #1F3274; ");
                     textOfBtn="O";
+                    highlightTheLabel('x');
                 }
                 else{
+                    targetedBtn.setStyle("-fx-text-fill: #febd08;-fx-background-color: #1F3274; ");
                     textOfBtn="X";
+                    highlightTheLabel('o');
                 }  
-                checkState();
+                targetedBtn.setText(textOfBtn); 
+                revieweTheBoaed();
             }
         }
     }
@@ -119,33 +119,27 @@ public class BoardScrController implements Initializable {
     private boolean isAnyRowNotifyWinningSomeone(){
         boolean flag = false;
         if(box1.getText().equals(box2.getText()) && box2.getText().equals(box3.getText()) && !box1.getText().equals("")){
-            HighlightThusBtns(box1, box2,box3);
+            highlightThusBtns(box1, box2,box3);
             if(box1.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } else if(box4.getText().equals(box5.getText()) && box5.getText().equals(box6.getText()) && !box4.getText().equals("")){
-            HighlightThusBtns(box4, box5,box6);
+            highlightThusBtns(box4, box5,box6);
             if(box4.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } else if(box7.getText().equals(box8.getText()) && box8.getText().equals(box9.getText()) && !box7.getText().equals("")){
-            HighlightThusBtns(box7, box8,box9);
+            highlightThusBtns(box7, box8,box9);
             if(box7.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } 
@@ -154,33 +148,27 @@ public class BoardScrController implements Initializable {
     private boolean isAnyColumnNotifyWinningSomeone(){
     boolean flag = false;
         if(box1.getText().equals(box4.getText()) && box4.getText().equals(box7.getText()) && !box1.getText().equals("")){
-            HighlightThusBtns(box1, box4,box7);
+            highlightThusBtns(box1, box4,box7);
             if(box1.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } else if(box2.getText().equals(box5.getText()) && box5.getText().equals(box8.getText()) && !box2.getText().equals("")){
-            HighlightThusBtns(box2, box5,box8);
+            highlightThusBtns(box2, box5,box8);
             if(box2.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } else if(box3.getText().equals(box6.getText()) && box6.getText().equals(box9.getText()) && !box3.getText().equals("")){
-            HighlightThusBtns(box3, box6,box9);
+            highlightThusBtns(box3, box6,box9);
             if(box3.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } 
@@ -190,23 +178,19 @@ public class BoardScrController implements Initializable {
     {
     boolean flag = false;
         if(box1.getText().equals(box5.getText()) && box5.getText().equals(box9.getText()) && !box1.getText().equals("")){
-            HighlightThusBtns(box1, box5,box9);
+            highlightThusBtns(box1, box5,box9);
             if(box1.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } else if(box3.getText().equals(box5.getText()) && box5.getText().equals(box7.getText()) && !box3.getText().equals("")){
-            HighlightThusBtns(box3, box5,box7);
+            highlightThusBtns(box3, box5,box7);
             if(box3.getText().equals("X")){
                 firstPllayerWin = true;
-                firstPlayerScore += 10;
             }else{
                 secondPlayerWin = true;
-                 secondPlayerScore += 10;
             }
             flag = true;
         } 
@@ -221,8 +205,7 @@ public class BoardScrController implements Initializable {
         }
         return flag;
     }
-    
-    private void checkState (){
+    private void revieweTheBoaed (){
         if (isAnyRowNotifyWinningSomeone()){
             someoneIsWin = true;
         }else if (isAnyColumnNotifyWinningSomeone()){
@@ -230,25 +213,72 @@ public class BoardScrController implements Initializable {
         }else if (isAnyDiagonalNotifyWinningSomeone()){
             someoneIsWin = true;
         }
-        
         if (someoneIsWin){
-            if(firstPllayerWin){
-                System.out.println("Player one is win");
-                HandleWinningSomeOne("x player");
-            }else 
-                System.out.println("Player two is win");
-            HandleWinningSomeOne("o player");
-            }
-        
-        if (isBoardFull()){
+                handleWinningSomeOne();
+        }else if (isBoardFull()){
             System.out.println("no one win..!");
-            // handle no one win
-        }  
+            // TODO :- HANDLE NO ONE WIN
+            handleNoOneWin();
+        }   
     } 
-    private void HighlightThusBtns(Button b1, Button b2,Button b3){
-        b1.setStyle("-fx-background-color:  #26bf2d; ");
-        b2.setStyle("-fx-background-color:  #26bf2d; ");
-        b3.setStyle("-fx-background-color:  #26bf2d; ");
+    private void highlightThusBtns(Button b1, Button b2,Button b3){
+        if(b1.getText().equals("X")){
+        b1.setStyle("-fx-text-fill: #febd08;-fx-background-color: #999b99; ");
+        b2.setStyle("-fx-text-fill: #febd08;-fx-background-color: #999b99; ");
+        b3.setStyle("-fx-text-fill: #febd08;-fx-background-color: #999b99; ");
+        }else{
+        b1.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #999b99; ");    
+        b2.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #999b99; ");
+        b3.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #999b99; ");
+        }
     }
-    public void HandleWinningSomeOne(String winner){} 
-}
+    public void handleWinningSomeOne(){
+            if(firstPllayerWin){
+                firstPlayerScore += 10;
+                highlightTheLabel('x');
+
+            }else if(secondPlayerWin){
+                secondPlayerScore += 10;
+                highlightTheLabel('o'); 
+            }
+            gameStatus = "finished";
+    } 
+    public void handleNoOneWin(){
+        playerX.setStyle("-fx-text-fill: #febd08;-fx-background-color: #1F3274; ");
+        playerO.setStyle("-fx-text-fill: #febd08;-fx-background-color: #1F3274; ");
+        ButtonType ok = new ButtonType("OK", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert a = new Alert(Alert.AlertType.NONE); 
+        a.setTitle("NO ONE WIN ..!");
+        a.setContentText("the game finished without winning any one");
+        a.getDialogPane().getButtonTypes().add(ok);
+        a.showAndWait();
+        setStandrdStart();
+    }
+    private void highlightTheLabel(char ch){
+         if (ch == 'x' || ch == 'X' ){
+            playerO.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #1F3274; ");
+            playerX.setStyle("-fx-text-fill: #febd08;-fx-background-color: #999b99; "); 
+         }else if(ch == 'o' || ch == 'O' ){
+            playerX.setStyle("-fx-text-fill: #febd08;-fx-background-color: #1F3274; ");
+            playerO.setStyle("-fx-text-fill: #ff4948;-fx-background-color: #999b99; ");
+         } 
+    }
+    private void setStandrdStart(){
+        textOfBtn = "X";
+        someoneIsWin = false;
+        firstPllayerWin = false ;
+        secondPlayerWin = false; 
+        box1.setText("");box1.setStyle("-fx-background-color: #1F3274; ");
+        box2.setText("");box2.setStyle("-fx-background-color: #1F3274; ");
+        box3.setText("");box3.setStyle("-fx-background-color: #1F3274; ");
+        box4.setText("");box4.setStyle("-fx-background-color: #1F3274; ");
+        box5.setText("");box5.setStyle("-fx-background-color: #1F3274; ");
+        box6.setText("");box6.setStyle("-fx-background-color: #1F3274; ");
+        box7.setText("");box7.setStyle("-fx-background-color: #1F3274; ");
+        box8.setText("");box8.setStyle("-fx-background-color: #1F3274; ");
+        box9.setText("");box9.setStyle("-fx-background-color: #1F3274; ");
+        someoneIsWin = false;
+         highlightTheLabel('o');
+    }
+    
+} 
