07688c85745de7a38a66c636cf2f6273f34a93f6
AHMHAM <ahmedhambouta@gmail.com>
2023-01-03 21:29:39 +0200

    Removed Build and Dist folders (They clutter commits)

--- ClientTicTacToe/src/clienttictactoe/BoardScrController.java
+++ ClientTicTacToe/src/clienttictactoe/BoardScrController.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clienttictactoe;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
