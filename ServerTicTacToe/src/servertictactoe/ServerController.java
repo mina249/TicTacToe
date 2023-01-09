d862f4c2b67eba5fd132efa5fcb92a85e3076795
AHMHAM <ahmedhambouta@gmail.com>
2023-01-10 01:36:07 +0200

    Server GUI Redesign

--- ServerTicTacToe/src/servertictactoe/ServerController.java
+++ ServerTicTacToe/src/servertictactoe/ServerController.java
@@ -5,21 +5,31 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 
 public class ServerController implements Initializable {
 
     @FXML
-    private Button ButtonStartServer;
+    private Button serverOnOffButton;
     @FXML
-    private Button ButtonStopServer;
+    private TableView<?> tableView;
+    @FXML
+    private TableColumn<?, ?> tableColumnName;
+    @FXML
+    private TableColumn<?, ?> tableColumnStatus;
+    @FXML
+    private TableColumn<?, ?> tableColumnScore;
+    @FXML
+    private TableColumn<?, ?> tableColumnNumGames;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
