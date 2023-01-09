33cf46885a69eda86265946ddb8b2bc5f97d02de
AHMHAM <ahmedhambouta@gmail.com>
2023-01-10 00:32:00 +0200

    Added Empty Database and Server Controller classes

---
+++ ServerTicTacToe/src/servertictactoe/ServerController.java
@@ -0,0 +1,25 @@
+package servertictactoe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class ServerController implements Initializable {
+
+    @FXML
+    private Button ButtonStartServer;
+    @FXML
+    private Button ButtonStopServer;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
