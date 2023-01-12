c15f59b53ca58ae0617dc2d0b9c1f899185370b8
asmaa727 <engineerasmaa2019@gmail.com>
2023-01-13 01:33:13 +0200

    replace game class with gamedate class

---
+++ ClientTicTacToe/src/clienttictactoe/GameDate.java
@@ -0,0 +1,35 @@
+
+package clienttictactoe;
+
+import java.time.LocalDateTime;
+
+
+public class GameDate
+{
+
+    private String  game_date;
+
+    public GameDate()
+    {
+        LocalDateTime now = LocalDateTime.now();
+        String game_date = now.toString();
+        String date=(String) game_date.toString().subSequence(0,16);
+        String rec_date=date.replaceAll(":","-");
+        this.game_date=rec_date;
+        
+    }
+
+    public String getGame_date() {
+        return game_date;
+    }
+
+    public void setGame_date() 
+    {
+        LocalDateTime now = LocalDateTime.now();
+        String game_date = now.toString();
+        String date=(String) game_date.toString().subSequence(0,16);
+        String reac_date=date.replaceAll(":","-");
+    }
+    
+}
+
